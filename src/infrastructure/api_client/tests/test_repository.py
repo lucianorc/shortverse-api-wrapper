7836290ce042c108c18ce5415d36ea081cb4d7c1
Luciano Costa <lcosta_96@hotmail.com.br>
2023-03-03 08:40:40 -0300

    Test fixture for repository created

--- src/infrastructure/api_client/tests/test_repository.py
+++ src/infrastructure/api_client/tests/test_repository.py
@@ -7,21 +7,20 @@ from infrastructure.api_client.repository import ApiRepository
 
 
 @pytest.fixture
-def api_client() -> APIClient:
-    return APIClient()
+def repo() -> ApiRepository:
+    client = APIClient()
+    return ApiRepository(client)
 
 
 @pytest.mark.vcr
-def test_if_repository_get_film_by_slug(api_client: APIClient):
-    repo = ApiRepository(api_client)
+def test_if_repository_get_film_by_slug(repo: ApiRepository):
     result = repo.get_film("anaconda")
 
     assert result.name == "Anaconda"
 
 
 @pytest.mark.vcr
-def test_if_repository_get_latest_films(api_client: APIClient):
-    repo = ApiRepository(api_client)
+def test_if_repository_get_latest_films(repo: ApiRepository):
     result = repo.get_latest_films()
 
     assert len(result) == 32
