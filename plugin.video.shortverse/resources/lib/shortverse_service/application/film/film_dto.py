70f32afe9e171f8e69ded33b0a9416fd2dec2eb7
Luciano Costa <lcosta_96@hotmail.com.br>
2023-03-04 12:01:21 -0300

    More attributes added to Film entity

--- plugin.video.shortverse/resources/lib/shortverse_service/application/film/film_dto.py
+++ plugin.video.shortverse/resources/lib/shortverse_service/application/film/film_dto.py
@@ -9,11 +9,14 @@ class FilmDTO(object):
     id: str
     slug: str
     name: str
-    description: str
+    director: str
+    short_description: str
+    long_description: str
     duration: int
     mature: bool
     source_url: str
     trailer_url: str
+    fanart: str
 
     available_at: datetime
     released_at: datetime
--- plugin.video.shortverse/resources/lib/shortverse_service/domain/film/entities.py
+++ plugin.video.shortverse/resources/lib/shortverse_service/domain/film/entities.py
@@ -1,17 +1,18 @@
-from dataclasses import dataclass
 from datetime import datetime
 
 
-@dataclass
 class Film(object):
     id: str
     slug: str
     name: str
-    description: str
+    director: str
+    short_description: str
+    long_description: str
     duration: int
     mature: bool
     source_url: str
     trailer_url: str
+    fanart: str
 
     available_at: datetime
     released_at: datetime
